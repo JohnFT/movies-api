bf2b76f854325b349fe50532a25758256a67810b
John <johnafonseca@gmail.com>
2019-06-18 01:21:12 -0500

    finall

--- src/sequelize.ts
+++ src/sequelize.ts
@@ -15,11 +15,11 @@ const properties = {
   models: [__dirname + '/src/models']
 } */
 export default new Sequelize({
-  username: 'kcxoatqmrxqmht',
-  password: '9e93ae19bd35550a1716d1da23a266c3f6096079b1b6456a65cfb2be5e469f68',
-  database: 'd1cm1n45ngf9hb',
+  username: 'ybdzsxgzqfvmez',
+  password: '7ddf76784245a867b71a5edb1098e9bd1c9c40db7f2d5a620394601e0c28f7cd',
+  database: 'd75upncbpkkgn3',
   dialect: 'postgres',
-  host: 'ec2-174-129-208-118.compute-1.amazonaws.com',
+  host: 'ec2-54-83-9-36.compute-1.amazonaws.com',
   dialectOptions: {
     ssl: true
   },
